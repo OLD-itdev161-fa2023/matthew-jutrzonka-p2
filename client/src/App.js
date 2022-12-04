2e85e541f213132a042155c439d4459e1adc0ba3
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2022-12-03 18:33:41 -0600

    Update App.js

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        Matthew Jutrzonka Project 2
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
